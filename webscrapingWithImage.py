0c4166fb09ec30ad3fe49c0ad98871fd9330a852
Brendan Lucas <brendanwflucas@gmail.com>
2019-03-06 17:54:47 -0500

    Fixed for python 3 and updated dependancies

--- webscrapingWithImage.py
+++ webscrapingWithImage.py
@@ -2,29 +2,43 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 from PIL import Image
+from io import BytesIO
 
-link = "https://www.reddit.com/"
+link = 'https://old.reddit.com/'
 
 def webscraper():
     opensite = urllib.request.urlopen(link)
+
     site_content = opensite.read()
     soup = BeautifulSoup(site_content, "lxml")
     soup.prettify()
-    posts = soup.select('div#siteTable div.thing')
-    print(posts[0].select("p.title a")[0].text)
-    print(posts[0].select("a.thumbnail img")[0].get("src"))
+    posts = soup.select('div#siteTable')
+    print(posts[0].select("a.title.may-blank")[0].text)
+    #same thing more specific
+    print(posts[0].select("div.entry.unvoted div.top-matter p.title a")[0].text)
 
+    #One way of getting an image and saving it to file then showing it
     image_name = "Image.jpg"
-    img1 = posts[0].select("a.thumbnail img")[0].get("src")
-    img1 = "http:" + img1
+    img1 = "https:" + posts[0].select("a img")[0].get("src")
+    # img1 = "https:" + img1
     TheImage = requests.get(img1)
 
     with open(image_name, "wb") as f:
         f.write(TheImage.content)
 
-
     image = Image.open(image_name)
     image.show()
+
+    #Or just show the image and use some shortcuts to use less code.
+    #getting the url for the route image
+    href = posts[0].select("div")[0].get("data-url")
+    #check the url
+    print(href)
+    #add the image and show it
+    sample_image = Image.open(BytesIO(requests.get(href).content))
+    sample_image.show()
+
+    #another way to show an image
 
 
 
